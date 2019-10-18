6333ac18c0936985f26fcbe670cd31d1ca0f168a
noteconvert <me@ashley.how>
2019-10-19 01:25:37 +0200

    add hub

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,14 @@ const date = new Date;
 const month = date.toLocaleString('default', { month: 'long' });
 
 monthText.innerHTML = month;
+
+if (month === 'January' || 'March' || 'May' || 'July' || 'August' || 'October' || 'December') {
+    console.log('31 days');
+    // update dom with correct days
+}
+else if (month === 'April' || 'June' || 'September' || 'November'){
+    console.log ('30 days');
+}
+else {
+    console.log('28 days');
+}
