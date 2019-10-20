9c532df860cfc6bf1685cd5557e501912a29cca4
noteconvert <me@ashley.how>
2019-10-20 18:28:56 +0200

    add check days

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,16 @@
 var monthText = document.getElementById('month');
+var calendarDate = document.getElementById('calendar-day');
 
 const date = new Date; 
 const month = date.toLocaleString('default', { month: 'long' });
+const day = date.getDate();
 
 monthText.innerHTML = month;
+
+var i;
+for (i = 0; i < (day - 1); i++) {
+    calendarDate.children[i].classList.add("checked-day");
+}
 
 if (month === 'January' || 'March' || 'May' || 'July' || 'August' || 'October' || 'December') {
     console.log('31 days');
