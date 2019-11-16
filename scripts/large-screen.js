d589bd5b364ff1082a628a9c40758c7171b7d25d
noteconvert <me@ashley.how>
2019-11-16 18:56:27 +0100

    add typed.js and delays

--- scripts/large-screen.js
+++ scripts/large-screen.js
@@ -1,13 +1,30 @@
-var screenProjects = document.getElementById('screen-projects');
+// var screenProjects = document.getElementById('screen-projects');
 var largeScreen = document.getElementById('large-screen');
 var explorerUrl = document.getElementById("explorer-url");
 
-function openProjects(){
+function openProjects() {
     largeScreen.style.display = "block";
     // console.log('open projects');
-    explorerUrl.innerText = `C:\\Users\\Ash\\Projects\\`;
+    // explorerUrl.innerText = `C:\\Users\\Ash\\Projects\\`;
+    document.getElementById('typed-url').innerHTML = `C:\\Users\\Ash\\Projects\\`;
+    // document.getElementById('typed-strings').style.display="block";
+    var typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        typeSpeed: 50,
+        showCursor: false,
+        startDelay: 2000
+    });
+
+    setTimeout(function () {
+        document.getElementById('project-tiles').style.display = "grid";
+    }, 3600);
+
+
 }
 
-function hideScreen(){
+function hideScreen() {
     largeScreen.style.display = "none";
 }
+
+
+
