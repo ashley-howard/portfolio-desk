17df36f8e56f0b37969d9d58e5d2f11dbcdf6df0
noteconvert <me@ashley.how>
2019-11-18 00:48:10 +0100

    fix hover on project tiles

--- scripts/large-screen.js
+++ scripts/large-screen.js
@@ -1,12 +1,12 @@
 // var screenProjects = document.getElementById('screen-projects');
 var largeScreen = document.getElementById('large-screen');
 var explorerUrl = document.getElementById("explorer-url");
+var prependTyped = document.getElementById('prepend-typed');
 
-function openProjects() {
+function openProjects(project) {
     largeScreen.style.display = "block";
-    document.getElementById('typed-url').innerHTML = `C:\\Users\\Ash\\Projects\\`;
     var typed = new Typed('#typed', {
-        stringsElement: '#typed-strings',
+        strings: ['C:\\Users\\Ash\\Projects\\'],
         typeSpeed: 50,
         showCursor: false,
         startDelay: 2000
@@ -16,12 +16,23 @@ function openProjects() {
         document.getElementById('project-tiles').style.display = "grid";
     }, 3600);
 
+    // PROJECT - HOME
+    if (project === "home") {
+        typed.destroy() // resets Typed
+        prependTyped.innerHTML = 'C:\\Users\\Ash\\Projects\\';
+        var typed = new Typed('#typed', {
+            strings: ['Breaking-the-second-wall'],
+            typeSpeed: 50,
+            showCursor: false,
+        });
+    }
 
 }
 
 function hideScreen() {
     largeScreen.style.display = "none";
     // reset everything
+    prependTyped.innerHTML = '';
     document.getElementById('typed').innerHTML = "";
     document.getElementById('project-tiles').style.display = "none";
 }
