b0b1549044d4eea87c85cb6aaeb12a51a48a82c7
noteconvert <me@ashley.how>
2019-12-01 02:40:30 +0100

    update coffee

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+function drinkCoffee() {
+    document.getElementById("coffee").classList.add("drink");
+
+    setTimeout(function () {
+        document.getElementById("coffee-note").style.textDecoration = "line-through";
+    }, 3200);
+}
