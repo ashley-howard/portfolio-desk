324363e2d391ab8f87f535ebc9b29d4a55dea6d7
noteconvert <me@ashley.how>
2019-12-15 15:25:14 +0100

    clean up scss, animations, and update hub

--- scripts/date.js
+++ scripts/date.js
@@ -6,23 +6,23 @@ const date = new Date;
 const month = date.toLocaleString('default', { month: 'long' });
 const day = date.getDate();
 
-monthText.innerHTML = month;
+// monthText.innerHTML = month;
 // taskbarTime.innerHTML = date.getHours() + ':' + date.getMinutes();
 
-function daysInThisMonth() {
-    // var now = new Date();
-    return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-}
+// function daysInThisMonth() {
+//     // var now = new Date();
+//     return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+// }
 
-for (var i = 0; i < (daysInThisMonth()); i++) {
-    var element = document.createElement("div");
-    element.setAttribute('class', 'grid-item');
-    element.innerHTML = i + 1;
-    if (day - 1 > i) {
-        element.classList.add("checked-day");
-    }
-    calendarDays.appendChild(element);
-}
+// for (var i = 0; i < (daysInThisMonth()); i++) {
+//     var element = document.createElement("div");
+//     element.setAttribute('class', 'grid-item');
+//     element.innerHTML = i + 1;
+//     if (day - 1 > i) {
+//         element.classList.add("checked-day");
+//     }
+//     calendarDays.appendChild(element);
+// }
 
 // (function () {
 //     function checkTime(i) {
