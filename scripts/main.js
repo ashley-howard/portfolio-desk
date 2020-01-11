25a7a7e9c5c1f232fb35c59215e074f1eb7a8d23
noteconvert <me@ashley.how>
2020-01-11 20:27:19 +0100

    add light switch functionality

--- scripts/main.js
+++ scripts/main.js
@@ -6,4 +6,41 @@ function drinkCoffee() {
     }, 3200);
 }
 
+var typed = new Typed('#intro-typed', {
+    stringsElement: '#intro-typed-strings',
+    typeSpeed: 75,
+    showCursor: false
+});
+
+const introScreen = document.getElementById('intro-screen');
+const lightSwitch = document.getElementById('light-switch')
+
+var firstTime = localStorage.getItem('firstTime');
+
+// load splash
+if (!firstTime) {
+    console.log("load splash")
+    localStorage.setItem('firstTime', 'true');
+    firstTime = localStorage.getItem('firstTime');
+}
+// dont load splash
+else {
+    console.log("don't load splash")
+    // introScreen.style.display = 'none' 
+    localStorage.setItem('firstTime', 'false');
+    firstTime = localStorage.getItem('firstTime');
+}
+
+function light(action) {
+    if (action === 'on') {
+        introScreen.style.display = 'flex';
+        lightSwitch.attributes.onclick.value = "light('off')"
+        lightSwitch.style.boxShadow = 'inset 0px -3px 2px #262626'
+    } else {
+        introScreen.style.display = 'none';
+        lightSwitch.attributes.onclick.value = "light('on')"
+        lightSwitch.style.boxShadow = 'inset 0px 3px 2px #262626'
+    }
+}
+
 
