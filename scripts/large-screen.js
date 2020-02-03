bf89e57ec3198c53c12636b7591ba4d0723bdf47
noteconvert <me@ashley.how>
2020-02-03 21:16:19 +0000

    clean up js

--- scripts/large-screen.js
+++ scripts/large-screen.js
@@ -1,49 +1,41 @@
-var screenProjects = document.getElementById('screen-projects');
-var screenAbout = document.getElementById('screen-about');
-var screenSkills = document.getElementById('screen-skills');
-var screenContact = document.getElementById('screen-contact');
-var largeScreen = document.getElementById('large-screen');
-var explorerUrl = document.getElementById("explorer-url");
-var prependTyped = document.getElementById('prepend-typed');
-var backButton = document.getElementById('back-button');
-var projectTiles = document.getElementById('project-tiles');
-var screenCache;
-var typed;
-var typedString;
-var typedStartDelay;
-var timeout;
-var screenName;
+const largeScreen = document.getElementById('large-screen');
+const prependTyped = document.getElementById('prepend-typed');
+const backButton = document.getElementById('back-button');
+const extendables = document.getElementsByClassName('extendable');
 
-function openScreen(screenName) {
+var screenCache, typed, typedString, typedStartDelay, timeout;
+
+function openScreen(screen) {
     extend()
+    document.getElementById('folders').style.display = "none";
 
-    if (screenName === 'projects') {
+    if (screen === 'projects') {
         timeout = 3900;
     }
 
-    else if (screenName === 'main') {
+    else if (screen === 'main') {
         timeout = 0;
     }
 
-    else if (screenName === 'about') {
+    else if (screen === 'about') {
         timeout = 3700;
     }
 
-    else if (screenName === 'skills') {
+    else if (screen === 'skills') {
         timeout = 3800;
     }
 
-    else if (screenName === 'contact') {
+    else if (screen === 'contact') {
         timeout = 3900;
     }
 
-    if (screenName === 'main') {
+    if (screen === 'main') {
         prependTyped.innerHTML = `https://ashley.how`;
     }
 
-    else if (screenCache === 'main' && screenName !== 'main') {
+    else if (screenCache === 'main' && screen !== 'main') {
         timeout = timeout - 3100;
-        typedString = `/${screenName}`
+        typedString = `/${screen}`
         typedStartDelay = 0
 
         var typed = new Typed('#typed', {
@@ -55,7 +47,7 @@ function openScreen(screenName) {
     }
 
     else {
-        typedString = `https://ashley.how/${screenName}`;
+        typedString = `https://ashley.how/${screen}`;
         typedStartDelay = 2000
 
         var typed = new Typed('#typed', {
@@ -67,62 +59,45 @@ function openScreen(screenName) {
     }
 
     setTimeout(function () {
-        if (screenName === 'main') {
+        if (screen === 'main') {
             screenCache.style.display = "none";
             document.getElementById('folders').style.display = "flex";
             prependTyped.innerHTML = 'https://ashley.how';
             backButton.style.display = "none";
             screenCache = 'main';
         }
-        
+
         else {
             typed.destroy()
             document.getElementById('folders').style.display = "none";
-            document.getElementById(`screen-${screenName}`).style.display = "block";
-            prependTyped.innerHTML = `https://ashley.how/${screenName}`;
-            screenCache = document.getElementById(`screen-${screenName}`);
+            document.getElementById(`screen-${screen}`).style.display = "block";
+            prependTyped.innerHTML = `https://ashley.how/${screen}`;
+            screenCache = document.getElementById(`screen-${screen}`);
             backButton.style.display = "flex";
             backButton.setAttribute("onclick", "openScreen('main')")
         }
     }, timeout);
 }
 
-
-
-
-
 function minimise() {
-    // largeScreen.style.display = "none";
-    // document.getElementById('folders').style.display = "none"
     shrink()
     document.getElementById('minimised').style.display = "flex";
-    largeScreen.style.bottom = "calc(100vh * 2)";
+    document.getElementById(screenCache.id).style.display = "none";
 }
 
 function maximise() {
-    // largeScreen.style.display = "block";
-    // document.getElementById('folders').style.display = "none"
+    extend()
     document.getElementById('minimised').style.display = "none";
-    largeScreen.style.bottom = "initial";
+    document.getElementById(screenCache.id).style.display = "block";
 }
 
 function closeScreen() {
-
     shrink()
+    document.getElementById(screenCache.id).style.display = "none";
     screenCache = undefined;
+    document.getElementById('folders').style.display = "flex";
     prependTyped.innerHTML = '';
-
-    // largeScreen.style.display = "none";
-    // reset everything
-    // document.getElementById('typed').innerHTML = "";
-    // projectTiles.style.display = "none";
-    // document.getElementById(`project-${projectCache}`).style.display = "none";
-    // backButton.style.display = "none";
 }
-
-// // use loop | put extendable on all elements that you want to extend
-// // create button to activate
-var extendables = document.getElementsByClassName('extendable')
 
 function extend() {
     var i;
@@ -136,5 +111,3 @@ function shrink() {
         extendables[i].classList.remove("extended");
     }
 }
-
-
