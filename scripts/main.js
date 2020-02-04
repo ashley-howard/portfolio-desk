6b15a31db3f20ee3c693e22a3f9d4e13febf5a91
noteconvert <me@ashley.how>
2020-02-04 20:12:56 +0000

    add better viewport

--- scripts/main.js
+++ scripts/main.js
@@ -45,4 +45,9 @@ function light(action) {
     localStorage.setItem('firstTime', 'false');
 }
 
-
+// We listen to the resize event
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
